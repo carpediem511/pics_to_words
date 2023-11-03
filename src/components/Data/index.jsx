34b230fd421636b8c244744d6e8c8256dfce12da
carpediem511 <vampire.0514@gmail.com>
2023-11-03 20:08:05 +0300

    изображения, почти готова разметка

---
+++ src/components/Data/index.jsx
@@ -0,0 +1,85 @@
+const DataArray = [
+
+	{
+		type: 'text',
+		content: 'Application',
+	},
+	{
+		type: 'image',
+		content: '/images/responsive-design.jpg',
+	},
+	{
+		type: 'image',
+		content: '/images/website.jpg',
+	},
+	{
+		type: 'text',
+		content: 'Programmer',
+	},
+	{
+		type: 'text',
+		content: 'Password',
+	},
+	{
+		type: 'image',
+		content: '/images/application.jpg',
+	},
+	{
+		type: 'image',
+		content: '/images/error.jpg',
+	},
+	{
+		type: 'text',
+		content: 'Server',
+	},
+	{
+		type: 'image',
+		content: '/images/design.jpg',
+	},
+	{
+		type: 'image',
+		content: '/images/password.png',
+	},
+	{
+		type: 'text',
+		content: 'Responsive design',
+	},
+	{
+		type: 'text',
+		content: 'Design',
+	},
+	{
+		type: 'image',
+		content: '/images/web-development.jpg',
+	},
+	{
+		type: 'text',
+		content: 'Keyboard',
+	},
+	{
+		type: 'image',
+		content: '/images/programmer.jpg',
+	},
+	{
+		type: 'image',
+		content: '/images/server.jpg',
+	},
+	{
+		type: 'image',
+		content: '/images/keyboard.jpg',
+	},
+	{
+		type: 'text',
+		content: 'Error',
+	},
+	{
+		type: 'text',
+		content: 'Web-development',
+	},
+	{
+		type: 'text',
+		content: 'Website',
+	},
+]
+
+export default DataArray
